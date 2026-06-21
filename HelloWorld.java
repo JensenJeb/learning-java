871b165c3b3c0b36215255ad481e5cf58f73a2d3
Jensen Jebasingh <89280780+JensenJeb@users.noreply.github.com>
2026-06-21 19:06:47 -0400

    forgot to add periods between the 'out' and 'println' and fixed the capitalization on 'system'

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
         double height = 5.5
         boolean learning = true
         System.out.println("my age is " + age);
-        system.out println("my height is " + height);
-        system.out println("am i learning? " + learning);
+        System.out.println("my height is " + height);
+        System.out.println("am i learning? " + learning);
     }
 }
