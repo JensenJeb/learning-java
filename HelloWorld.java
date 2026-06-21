d303ae8673ff13a062a9c42f9814c3fe5b480eea
Jensen Jebasingh <89280780+JensenJeb@users.noreply.github.com>
2026-06-21 19:14:14 -0400

    completed the basics of varibles and data types!

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,12 @@ public class HelloWorld {
         int age = 17;
         double height = 5.5;
         boolean learning = true;
+        char initial = j;
+        String name = jensen;
         System.out.println("my age is " + age);
         System.out.println("my height is " + height);
         System.out.println("am i learning? " + learning);
+        System.out.println("what's the first initial of my name? " + initial);
+        System.out.println("whats my name? " + name);
     }
 }
