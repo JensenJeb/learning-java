de24f3f98cb28ecd4f4ab6491bb55465740e6565
Jensen Jebasingh <89280780+JensenJeb@users.noreply.github.com>
2026-06-21 18:59:20 -0400

    add age variable

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+        int age = 16;
+        System.out.println("my age is " + age);
     }
 }
