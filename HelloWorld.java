292b1c4a591194915ffccf28a5c5eca9f8cd1e38
Jensen Jebasingh <89280780+JensenJeb@users.noreply.github.com>
2026-06-21 19:07:33 -0400

    added semi colon to double and boolean

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         int age = 17;
-        double height = 5.5
-        boolean learning = true
+        double height = 5.5;
+        boolean learning = true;
         System.out.println("my age is " + age);
         System.out.println("my height is " + height);
         System.out.println("am i learning? " + learning);
