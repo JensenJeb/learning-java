c8f567d640b7f25f2506bb1e9080ab693aefb5ae
Jensen Jebasingh <89280780+JensenJeb@users.noreply.github.com>
2026-06-22 10:05:19 -0400

    I created the strings for printing out basic mathamatics using varibles that equal a whole number

--- Basics/Operators.java
+++ Basics/Operators.java
@@ -1,5 +1,12 @@
 public class Operators {
     public static void main(string[]args){
+        int a = 10;
+        int b = 3;
         
+        System.out.println( "a + b  = " + (a + b =));
+        System.out.println( "a - b  = " + (a - b =));
+        System.out.println( "a * b  = " + (a * b =));
+        System.out.println( "a / b  = " + (a / b =));
+        System.out.println( "a % b  = " + (a % b =));
     }
 }
