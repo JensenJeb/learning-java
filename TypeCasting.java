036103f2c4772d448cc5ae0debb2ebe7c4b5b531
Jensen Jebasingh <89280780+JensenJeb@users.noreply.github.com>
2026-06-22 09:45:02 -0400

    capitalized System

--- TypeCasting.java
+++ TypeCasting.java
@@ -2,6 +2,6 @@ public class TypeCasting {
     public static  void  main(String[]args){
         double price = 19.99;
         int wholedollar = (int) price;
-        system.out.println("price as an int: " + wholedollar);
+        System.out.println("price as an int: " + wholedollar);
     }
 }
